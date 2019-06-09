366166ef7ba558c45e04414689223419647258ce
Raffi <rmanisalian@cinemoz.com>
2019-06-09 15:31:54 +0300

    tasks vc now inherits base vc

--- Musicia/tasksViewController.swift
+++ Musicia/tasksViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class tasksViewController: UIViewController {
+class tasksViewController: baseViewController {
     @IBOutlet weak var tableView: UITableView!
     
     var items: Dictionary<String, Dictionary<String, Any>>? {
